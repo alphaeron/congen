b77d06e080cc003ad47fd5618d139171b3db7198
John Matty <jcmjohnmatty@me.com>
2026-04-25 10:45:55 -0400

    Add lat exercises

---
+++ backend/resources/migrations/data/exercises/changelog-2026-04-25_13_supinated_lat_pulldown.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset John Matty:1 labels:prod,test
+--comment: Add exercise Supinated Lat Pulldown
+--rollback SELECT 1
+
+INSERT INTO exercise (name, description, movement_type, is_unilateral, is_upper, is_accessory)
+VALUES (
+  'Supinated Lat Pulldown',
+  'Grasp the bar with an underhand (supinated) grip.  Pull the shoulder blades down and back, bringing the bar down to your upper chest.  Slowly return the bar to the starting position, resisting the urge to let the weight drop.  Keep your torso upright or with a slight 10–20 degree lean back, avoiding excessive leaning or momentum throughout the exercise.',
+  'vertical_pull',
+  false,
+  true,
+  true
+);
+INSERT INTO exercise_muscle (exercise_name, muscle_name) VALUES
+  ('Supinated Lat Pulldown', 'lats');
+INSERT INTO exercise_equipment (exercise_name, equipment_name) VALUES
+  ('Supinated Lat Pulldown', 'Lat Pulldown');
+INSERT INTO exercise_workout_type (exercise_name, movement_type, workout_type) VALUES
+  ('Supinated Lat Pulldown', 'vertical_pull', 'dynamic_effort'),
+  ('Supinated Lat Pulldown', 'vertical_pull', 'maximal_effort');
+
